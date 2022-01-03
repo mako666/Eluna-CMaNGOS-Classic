9a0c813c21eba3e2cf8ab4bbe601427c32441c4f
Niam5 <ebakkedahl@gmail.com>
2022-01-03 09:04:48 -0600

    Revert "Update revision_sql.h"
    This reverts commit ef07962a20ebd8419dfab60af57c88ff1957f079.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
- #define REVISION_DB_REALMD "required_14039_01_realmd_anticheat"
+ #define REVISION_DB_REALMD "required_z2778_01_realmd_anticheat"
  #define REVISION_DB_LOGS "required_z2778_01_logs_anticheat"
  #define REVISION_DB_CHARACTERS "required_z2775_01_characters_raf"
  #define REVISION_DB_MANGOS "required_z2783_01_mangos_spawn_groups"
